325e50748c3dae945ca987a4f395f0d7f679caeb
athulvinayakumar <athulvinayakumar2023b@mca.ajce.in>
2023-11-14 16:16:25 +0530

    netflix

---
+++ src/components/Banner.jsx
@@ -0,0 +1,31 @@
+ import React, { useEffect, useState } from 'react'
+ import './Banner.css'
+import instance from '../instance';
+ function Banner({fetchUrl}) {
+   //  console.log(fetchUrl);
+   const base_url = "https://image.tmdb.org/t/p/original/";
+   const [movie,setMovie] = useState();
+     
+    const fetchData =async ()=>{
+       const {data}= await instance.get(fetchUrl)
+       setMovie(data.results[Math.floor(Math.random()*data.results.length)]);
+    }
+console.log(movie);
+useEffect(()=>{
+    fetchData()
+},[]) 
+   return (
+     <div style={{height:'600px',backgroundImage:`url(${base_url}${movie?.backdrop_path
+     }`,backgroundAttachment:'fixed',backgroundPosition:'center'}}>
+   <div className='banner-content'>
+      <h1>{movie?.name}</h1>
+      <button className='btn btn-danger'>Play <i class="fa-solid fa-play"></i></button>
+      <button className='btn border-white ms-2 more'>More info <i class="fa-solid fa-caret-down"></i></button>
+      <h2>{movie?.overview?.slice(0,200)}..</h2>
+   </div>
+     </div>
+
+   )
+ }
+ 
+ export default Banner
